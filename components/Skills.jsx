944d8f477ef0be3bf160cfd02034c784821b9403
BleriotWafo <gabinwafo@yahoo.com>
2023-01-09 13:50:53 +0100

    ""

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -9,7 +9,7 @@ import Github from '../public/assets/skills/github1.png';
 import Firebase from '../public/assets/skills/firebase.png';
 import NextJS from '../public/assets/skills/nextjs.png'
 import AWS from '../public/assets/skills/aws.png';
-import java from '../public/assets/skills/java-Logo.png'
+import java from '../public/assets/skills/java.png'
 
 const Skills = () => {
   return (
